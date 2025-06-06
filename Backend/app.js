e771c47ca4a88b3f63f35f49a45cd95115e93306
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-06 10:13:20 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,42 +1,14 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const bodyParser = require('body-parser');
-// const connectToDb = require("./db/db");
-// const authRoutes = require("./routes/authRoutes");
-// const UserRoutes=require('./routes/UserRoutes');
-
-// connectToDb();
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use("/api/auth", authRoutes);
-// app.use('/api/user',UserRoutes);
-
-// module.exports = app;
-
-
-
-
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser');
 const connectToDb = require("./db/db");
 const authRoutes = require("./routes/authRoutes");
-const UserRoutes = require("./routes/UserRoutes");
+const UserRoutes=require('./routes/UserRoutes');
 
-const app = express();
 connectToDb();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -44,9 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", UserRoutes);
+app.use('/api/user',UserRoutes);
 
-// ✅ Serve React frontend
 const __dirname = path.resolve(); // important in ES modules
 app.use(express.static(path.join(__dirname, "dist")));
 
@@ -55,3 +26,7 @@ app.get("*", (req, res) => {
 });
 
 module.exports = app;
+
+
+
+
