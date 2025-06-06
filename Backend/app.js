48c1e56070c517f941406f4a1f92d36ce53eb100
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-06 09:54:53 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,26 +19,3 @@ app.use("/api/auth", authRoutes);
 app.use('/api/user',UserRoutes);
 
 module.exports = app;
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const connectToDb = require("./db/db");
-const authRoutes = require("./routes/authRoutes");
-const UserRoutes=require('./routes/UserRoutes');
-
-connectToDb();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/api/auth", authRoutes);
-app.use('/api/user',UserRoutes);
-
-module.exports = app;
-
-
